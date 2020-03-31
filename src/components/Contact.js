e451c04d20554ede0c74fdb29db95e81ff66d17d
WildSteph <61732693+WildSteph@users.noreply.github.com>
2020-03-31 11:41:10 +0200

    modif contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,27 @@
 import React from "react";
-const ISOnline = true
-const Contact = () =>  <div className = "Contact">
-<img  className="avatar" src ="https://cdn.discordapp.com/attachments/221721609436790784/694198643791495208/EOSwdDtU4AA8-sc.jpg" alt="" />
-  <div>
-    <h1 className ="name">Hello</h1>
-    <p className="status">{ISOnline ? <p><span className="status-online"></span>online</p> : <p className ="status-offline"><span className="status-online"></span>offline</p>}</p>
+const ISOnline = true;
+const Contact = () => (
+  <div className="Contact">
+    <img
+      className="avatar"
+      src="https://cdn.discordapp.com/attachments/221721609436790784/694198643791495208/EOSwdDtU4AA8-sc.jpg"
+      alt=""
+    />
+    <div>
+      <h1 className="name">Stephaine PANTIEZ</h1>
+      <p className="status">
+        {ISOnline ? (
+          <p>
+            <span className="status-online" />online
+          </p>
+        ) : (
+          <p className="status-offline">
+            <span className="status-online" />offline
+          </p>
+        )}
+      </p>
+    </div>
   </div>
-</div> 
+);
 
-export default Contact
+export default Contact;
