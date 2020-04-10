164f4eee4ff1ea0c8c4a7de0904ce42398a4161d
WildSteph <Stephaine.Pantiez@gmail.com>
2020-04-10 16:30:38 +0200

    ajoute de tout les fichée comme un gros sac

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,28 @@
 import React from "react";
 import "./styles.css";
 
-const Contact = function(props) {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.avatar} alt="" />
-      <div>
-        <h4 className="name">{props.name}</h4>
+class Contact extends React.Component  {
+  constructor(props) {
+    super(props);
+      this.state={
+        online : true
+     }
+    }
+  render(){
+    return <div className="Contact">
+        <img className="avatar" src={this.props.avatar} alt="" />
+      <div onClick={event => {
+            const newonline =!this.state.online;
+            this.setState({ online : newonline });
+            }} >
+          <h4 className="name" >{this.props.name}</h4>
         <div className="status">
-          <span className={props.online ? "status-online" : "status-offline"} />
-          <p className="status-text"> {props.online ? "online" : "offline"} </p>
+            <span className={this.state.online ? "status-online" : "status-offline"} />
+            <p className="status-text" > {this.state.online ? "online" : "offline"} </p>
+          </div>
         </div>
-      </div>
     </div>
-  );
+    }
 };
 
 export default Contact;
